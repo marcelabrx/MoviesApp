544285f723a6248f662d051fb1b3936b3ef53e1c
Marcela Britos <marcelal.britos@gmail.com>
2023-08-28 18:58:00 -0300

    creating  branch layout-components and adding components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <p>© 2023 Movies App. All rights reserved.</p>
+            <p>Created By Marcela Britos & Indra Lima</p>
+        </footer>
+
+    )
+}
+
+export default Footer
