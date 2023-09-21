128d3edff37f1356bab75d985b65b0a93bd77ec0
Indra Lima <indraalima@gmail.com>
2023-09-21 00:16:04 -0300

    feature navigation link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,102 +1,121 @@
-import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import TheaterComedyTwoToneIcon from '@mui/icons-material/TheaterComedyTwoTone';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
-import SearchIcon from '@mui/icons-material/Search';
-import InputBase from '@mui/material/InputBase';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import SearchIcon from "@mui/icons-material/Search";
+import InputBase from "@mui/material/InputBase";
+import { Link } from "react-router-dom";
 
+const pages = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Last Movies",
+    path: "/lastMovies",
+  },
+  {
+    name: "Popular Movies",
+    path: "/popularMovies",
+  },
+  {
+    name: "Favorites",
+    path: "/favoritesMovies",
+  },
+];
 
-const pages = ['Home', 'Last Movies', 'Popular Movies','Favorites'];
-const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(1),
-      width: 'auto',
-    },
-  }));
-  
-  const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }));
-  
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        width: '12ch',
-        '&:focus': {
-          width: '20ch',
-        },
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
       },
     },
+  },
 }));
 
 function Header() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
 
-
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
-  }; 
+  };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
 
-
   return (
-    <AppBar position="static" sx={{backgroundColor:"#ab526b", height:"13vh", paddingTop:"12px", fontFamily:"Luckiest Guy"}}>
+    <AppBar
+      position="static"
+      sx={{
+        backgroundColor: "#ab526b",
+        height: "13vh",
+        paddingTop: "12px",
+        fontFamily: "Luckiest Guy",
+      }}
+    >
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          {/* <Logo sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} /> */}
           <Typography
             variant="h6"
             noWrap
             component="a"
             href="/"
-            
             sx={{
               mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'Luckiest Guy',
-              fontSize: '40px',
-              letterSpacing: '.3rem',
-              color: '#fff',
-              textDecoration: 'none',
+              display: { xs: "none", md: "flex" },
+              fontFamily: "Luckiest Guy",
+              fontSize: "40px",
+              letterSpacing: ".3rem",
+              color: "#fff",
+              textDecoration: "none",
             }}
           >
             Movies Mania
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -111,26 +130,30 @@ function Header() {
               id="menu-appbar"
               anchorEl={anchorElNav}
               anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
+                vertical: "bottom",
+                horizontal: "left",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
+                vertical: "top",
+                horizontal: "left",
               }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: 'block', md: 'none' },
-                // fontFamily: 'Luckiest Guy'
+                display: { xs: "block", md: "none" },
               }}
             >
               {pages.map((page) => (
-                <MenuItem className='bg-menu' key={page} onClick={handleCloseNavMenu} sx={{backgroundColor: "#bca297", color:"white"}}>
-                   <Link to={`${page}`}  style={{textDecoration:"none"}}>
-                  <Typography textAlign="center">{page}</Typography>
-                   </Link> 
+                <MenuItem
+                  className="bg-menu"
+                  key={page.path}
+                  onClick={handleCloseNavMenu}
+                  sx={{ backgroundColor: "#bca297", color: "white" }}
+                >
+                  <Link to={`${page.path}`} style={{ textDecoration: "none" }}>
+                    <Typography textAlign="center">{page.name}</Typography>
+                  </Link>
                 </MenuItem>
               ))}
             </Menu>
@@ -142,37 +165,42 @@ function Header() {
             href="/"
             sx={{
               mr: 2,
-              display: { xs: 'flex', md: 'none' },
+              display: { xs: "flex", md: "none" },
               flexGrow: 1,
-              fontFamily: 'Luckiest Guy',
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              fontFamily: "Luckiest Guy",
+              // fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
             Movies Mania
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
-              <Link to={`${page}`} key={page} style={{textDecoration:"none"}}>
-              <Button
-                onClick={handleCloseNavMenu}
-                sx={{ mx: 3, my: 2, color: 'white', display: 'block' }}
+              <Link
+                to={`${page.path}`}
+                key={page.path}
+                style={{ textDecoration: "none" }}
               >
-                {page}
-              </Button>
+                <Button
+                  onClick={handleCloseNavMenu}
+                  sx={{ mx: 3, my: 2, color: "white", display: "block" }}
+                >
+                  {page.name}
+                </Button>
               </Link>
             ))}
           </Box>
-        <Search>
+          <Search>
             <SearchIconWrapper>
-            <SearchIcon />
+              <SearchIcon />
             </SearchIconWrapper>
             <StyledInputBase
-            placeholder="Search…"
-            inputProps={{ 'aria-label': 'search' }}
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
             />
-        </Search>
+          </Search>
         </Toolbar>
       </Container>
     </AppBar>
